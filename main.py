6754637c26904550084daae993b7bc59937503aa
pranikalimbu <141321142+pranikalimbu@users.noreply.github.com>
2026-02-04 14:03:05 +0545

    Add Naive Bayes model

---
+++ main.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import joblib
+
+st.title("Nepali News Category Classifier")
+
+
+model = joblib.load("nepali_news_nb_model.joblib")
+st.sidebar.success("Model loaded successfully!")
+
+
+input_text = st.text_area(
+    label="Enter News Article Text",
+    height=200,
+    max_chars=1000
+)
+
+if st.button("Predict Category"):
+    if input_text.strip():
+        
+            prediction = model.predict([input_text])[0]
+            st.success(f"The predicted category is: {prediction}")
+        
+    else:
+        st.warning("Please enter some text first!")
